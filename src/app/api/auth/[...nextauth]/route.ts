1f4107a20354501311aa2e1f0be7f07496cd5fda
jhlee <wisdom_13_@naver.com>
2023-08-02 21:47:02 +0900

    카카오 로그인

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,53 @@
+import NextAuth, { NextAuthOptions } from "next-auth"
+import KakaoProvider from 'next-auth/providers/kakao'
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID!,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET!,
+    }),
+  ],
+
+  // callbacks: {
+  //   async signIn({ user: { id, name, image, email } }) {
+  //     if (!email) {
+  //       return false;
+  //     }
+  //     addUser({
+  //       id,
+  //       name: name || '',
+  //       image,
+  //       email,
+  //       username: email.split('@')[0]
+  //     });
+  //     return true;
+  //   },
+
+  //   async session({ session, token }) {
+  //     const user = session?.user;
+  //     if (user) {
+  //       session.user = {
+  //         ...user,
+  //         username: user.email?.split('@')[0] || '',
+  //         id: token.id as string
+  //       }
+  //     }
+  //     return session
+  //   },
+
+  //   async jwt({ token, user }) {
+  //     if (user) {
+  //       token.id = user.id;
+  //     }
+  //     return token;
+  //   }
+  // },
+  // pages: {
+  //   signIn: '/auth/signin',
+  // }
+}
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-import Dudge from '@/components/dudge'
-import Image from 'next/image'
+'use client'
+
+import { signIn, signOut, useSession } from "next-auth/react"
 
 export default function Home() {
+  const { data: session } = useSession();
   return (
     <>
       <div>Main</div>
+      {
+        session ? (
+          <div>
+            <p>{session.user?.name} 님 안녕</p>
+            <button onClick={() => signOut()}>로그아웃</button>
+          </div>
+        ) : (
+          <button onClick={() => signIn()}>카카오 로그인</button>
+        )
+      }
     </>
   )
 }
