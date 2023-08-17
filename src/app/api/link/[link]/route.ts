43c624b7da382998bcd6d9fb0f6d444d67ce2385
jhlee <wisdom_13_@naver.com>
2023-08-18 04:27:01 +0900

    링크생성 - 서버 (업데이트)

--- src/app/api/link/[link]/route.ts
+++ src/app/api/link/[link]/route.ts
@@ -4,6 +4,8 @@ import { NextRequest, NextResponse } from 'next/server';
 type Context = {
   params: { link: string };
 };
+
+// 링크 중복 검사
 export async function GET(_: NextRequest, context: Context) {
   const link = context.params.link;
 
--- src/app/api/link/route.ts
+++ src/app/api/link/route.ts
@@ -1,5 +1,22 @@
-import { NextResponse } from 'next/server';
+import { settingLink } from '@/service/user';
+import { withSessionUser } from '@/util/session';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET() {
   return NextResponse.json('');
+}
+
+// 링크 생성
+export async function PUT(req: NextRequest) {
+  return withSessionUser(async (user) => {
+    const { link } = await req.json();
+
+    if (link == null) {
+      return new Response('Bad Request', { status: 400 });
+    }
+
+    return settingLink(user.id, link)
+      .then(res => NextResponse.json(res))
+      .catch(error => new Response(JSON.stringify(error), { status: 500 }))
+  })
 }
