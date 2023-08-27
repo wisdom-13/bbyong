2e2a19b76b3d847b1a81581ba35b93d1e4f7df09
jhlee <wisdom_13_@naver.com>
2023-08-27 18:19:24 +0900

    메인 페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,34 @@
 'use client'
 
 import Button from '@/components/ui/Button';
+import Title from '@/components/ui/Title';
 import { signIn, signOut, useSession } from "next-auth/react";
 import Image from 'next/image';
+import Link from 'next/link';
+import useSWR from 'swr';
 
 
 export default function Home() {
   const { data: session } = useSession();
+  const user = session?.user;
 
   return (
     <>
-      {
-        session ? (
-          <div>
-            <p>{session.user?.name}의 두더지</p>
-            <Image src='/mole.png' width={300} height={300} alt='mole' />
-            <Button text='마이페이지' />
-            <Button text='로그아웃' onClick={() => signOut()} />
-          </div>
-        ) : (
-          <Button text='시작하기' onClick={() => signIn()} />
-        )
-      }
+      {user && <Title text='Home' />}
+      {!user && <Title text='뿅' />}
+      <div className='flex flex-col gap-3 p-6'>
+        {
+          user ? (
+            <>
+              <Image src='/mole.png' width={300} height={300} alt='mole' />
+              <Link href={user.link ? `/${user.link}` : '/setting/link'}>내 두더지집</Link>
+              <Button text='로그아웃' onClick={() => signOut()} />
+            </>
+          ) : (
+            <Button text='시작하기' onClick={() => signIn()} />
+          )
+        }
+      </div>
     </>
   )
 }
