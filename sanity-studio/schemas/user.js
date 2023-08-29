7af22d2eb5903fb5bd838bc5c2459a21076836ae
jhlee <wisdom_13_@naver.com>
2023-08-30 05:08:06 +0900

    user 스키마 업데이트 - bio, links 필드 추가

--- sanity-studio/schemas/user.js
+++ sanity-studio/schemas/user.js
@@ -28,6 +28,45 @@ export default {
       name: 'dudge',
       type: 'number'
     },
+    {
+      title: 'Bio',
+      name: 'bio',
+      type: 'string',
+    },
+    {
+      title: 'Links',
+      name: 'link',
+      type: 'array',
+      of: [
+        {
+          title: 'Link',
+          name: 'link',
+          type: 'document',
+          fields: [
+            {
+              title: 'Title',
+              name: 'title',
+              type: 'string',
+            },
+            {
+              title: 'Url',
+              name: 'url',
+              type: 'string',
+            },
+            {
+              title: 'Click',
+              name: 'click',
+              type: 'number',
+            },
+            {
+              title: 'IsUse',
+              name: 'isUse',
+              type: 'boolean',
+            }
+          ]
+        }
+      ]
+    }
   ],
   preview: {
     select: {
