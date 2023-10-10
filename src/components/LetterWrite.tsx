5987fe79b64b8b4e3b2273797753abfd7b4db80d
jhlee <wisdom_13_@naver.com>
2023-10-10 23:59:19 +0900

    작성 스타일링

--- src/components/LetterWrite.tsx
+++ src/components/LetterWrite.tsx
@@ -1,3 +1,5 @@
+import Button from './ui/Button';
+import Input from './ui/Input';
 import Title from './ui/Title';
 
 type Props = {
@@ -6,8 +8,17 @@ type Props = {
 
 export default function LetterWrite({ address }: Props) {
   return (
-    <div>
+    <div className=' text-sm'>
       <Title text='편지 작성' />
+      <div className='px-6'>
+        <form>
+          <Input className='mb-3' placeholder='이름' />
+          <Input className='mb-3' placeholder='제목' />
+          <Input className='mb-3' textarea={true} placeholder='내용' />
+          <Button color='blue' text='작성하기' />
+        </form>
+      </div>
+
     </div>
   );
 }
