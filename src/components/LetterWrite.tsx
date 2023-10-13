c9677b6eff232d4f0e257d5bd485854f279527fe
jhlee <wisdom_13_@naver.com>
2023-10-13 16:55:20 +0900

    편지 작성 폼 검증

--- src/components/LetterWrite.tsx
+++ src/components/LetterWrite.tsx
@@ -1,21 +1,87 @@
+'use client'
+
+import { useForm } from 'react-hook-form';
 import Button from './ui/Button';
 import Input from './ui/Input';
 import Title from './ui/Title';
+import { useState } from 'react';
+import ErrorMsg from './ui/ErrorMsg';
+import { UserDetail } from '@/model/user';
+import useSWR from 'swr'
 
 type Props = {
   address: string;
 }
 
+interface HookFormTypes {
+  title: string;
+  name: string;
+  content: string;
+}
+
 export default function LetterWrite({ address }: Props) {
+  const { data: user } = useSWR<UserDetail>(`/api/profile/${address}`);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { register, formState: { errors }, handleSubmit, reset } = useForm<HookFormTypes>({
+    mode: 'onBlur',
+    shouldFocusError: false,
+  });
+
+  if (!user) {
+    return false;
+  }
+
+  const onSubmit = (data: HookFormTypes) => {
+    setIsLoading(true);
+    console.log(user.id)
+  }
+
+  const nameChk = register('name', {
+    required: true,
+    minLength: {
+      value: 2,
+      message: "2자 이상의 이름을 입력해주세요."
+    },
+  });
+
+  const titleChk = register('title', {
+    required: true,
+    minLength: {
+      value: 2,
+      message: "2자 이상의 제목을 입력해주세요."
+    },
+  });
+
+  const contentChk = register('content', {
+    required: true,
+    minLength: {
+      value: 10,
+      message: "10자 이상의 내용을 입력해주세요."
+    },
+  });
+
+
   return (
     <div className=' text-sm'>
       <Title text='편지 작성' />
       <div className='px-6'>
-        <form>
-          <Input className='mb-3' placeholder='이름' />
-          <Input className='mb-3' placeholder='제목' />
-          <Input className='mb-3' textarea={true} placeholder='내용' />
-          <Button color='blue' text='작성하기' />
+        <h3 className='font-bold text-2xl mb-5'>To. {user?.name}</h3>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label className='block mb-3'>
+            <Input placeholder='이름' register={nameChk} />
+            <ErrorMsg msg={errors.name?.message} />
+          </label>
+          <label className='block mb-3'>
+            <Input placeholder='제목' register={titleChk} />
+            <ErrorMsg msg={errors.title?.message} />
+          </label>
+          <label className='block mb-3'>
+            <Input textarea={true} placeholder='내용' register={contentChk} />
+            <ErrorMsg msg={errors.content?.message} />
+          </label>
+          <Button color='blue' text='작성하기' isLoading={isLoading} />
         </form>
       </div>
 
