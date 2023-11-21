43ff1c33dd73bbdd3cfba2339428aef2539801a2
jhlee <wisdom_13_@naver.com>
2023-11-21 19:10:35 +0900

    두더지 만들기 - 아이템 불러오기

--- src/components/SettingMoleMenu.tsx
+++ src/components/SettingMoleMenu.tsx
@@ -9,7 +9,7 @@ import { useRef, useState } from 'react';
 
 export default function SettingMoleMenu() {
   const moleImageRef = useRef<HTMLDivElement>(null);
-  const [tab, setTab] = useState('1');
+  const [tab, setTab] = useState(1);
 
   const onDownload = () => {
     if (!moleImageRef.current) return
@@ -17,6 +17,20 @@ export default function SettingMoleMenu() {
       saveAs(blob, 'card.png');
     });
   };
+
+  const tmpItemMenu = ['눈', '머리', '옷', '장식'];
+
+  const tmpItemList = (menu: Number) => {
+    let arr = [];
+    for (let i = 1; i <= 5; i++) {
+      arr.push(
+        <div className='w-[80px] h-[80px] bg-blue-100'>
+          <Image src={`/moleItem/type${menu}/${menu}-${i}.png`} width={80} height={80} alt={`item${menu}-${i}`} />
+        </div>
+      )
+    }
+    return arr;
+  }
 
   return (
     <div>
@@ -26,80 +40,21 @@ export default function SettingMoleMenu() {
           <div className='w-[80px] h-[80px] bg-blue-100'>item</div>
         </div>
       </div>
-      <div className='flex mt-3'>
-        <div className='w-[80px] h-10 bg-red-100' onClick={() => setTab('1')}>메뉴1</div>
-        <div className='w-[80px] h-10 bg-red-100' onClick={() => setTab('2')}>메뉴2</div>
-        <div className='w-[80px] h-10 bg-red-100' onClick={() => setTab('3')}>메뉴3</div>
-        <div className='w-[80px] h-10 bg-red-100' onClick={() => setTab('4')}>메뉴4</div>
+
+      <div className='flex mt-3 items-center h-10'>
+        {
+          tmpItemMenu.map((menu, i) => (
+            <div key={i} className='w-[80px] h-full bg-red-100' onClick={() => setTab(i)}>{menu}</div>
+          ))
+        }
       </div>
-      {
-        tab == '1' &&
-        <div className='mt-3'>
-          <div className='grid grid-cols-4 gap-4'>
 
-            <div className='w-[80px] h-[80px] bg-blue-100'>
-              <Image src={`/moleItem/type1/1-1.png`} width={80} height={80} alt='item1-1' />
-            </div>
+      <div className='mt-3'>
+        <div className='grid grid-cols-4 gap-4'>
+          {tmpItemList(tab + 1)}
+        </div>
+      </div>
 
-            <div className='w-[80px] h-[80px] bg-blue-100'>item1</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item1</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item1</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item1</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item1</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item1</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item1</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item1</div>
-          </div>
-        </div>
-      }
-      {
-        tab == '2' &&
-        <div className='mt-3'>
-          <div className='grid grid-cols-4 gap-4'>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item2</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item2</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item2</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item2</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item2</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item2</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item2</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item2</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item2</div>
-          </div>
-        </div>
-      }
-      {
-        tab == '3' &&
-        <div className='mt-3'>
-          <div className='grid grid-cols-4 gap-4'>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item3</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item3</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item3</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item3</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item3</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item3</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item3</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item3</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item3</div>
-          </div>
-        </div>
-      }
-      {
-        tab == '4' &&
-        <div className='mt-3'>
-          <div className='grid grid-cols-4 gap-4'>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item4</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item4</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item4</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item4</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item4</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item4</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item4</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item4</div>
-            <div className='w-[80px] h-[80px] bg-blue-100'>item4</div>
-          </div>
-        </div>
-      }
       <div className='mt-3'>
         <Button text='저장' onClick={onDownload} />
       </div>
