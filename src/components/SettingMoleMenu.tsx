e860e323f9e4e2b0e3800318b185adf21a648bf6
jhlee <wisdom_13_@naver.com>
2023-11-26 14:07:33 +0900

    아이템 객체 형식 변경

--- src/components/SettingMoleMenu.tsx
+++ src/components/SettingMoleMenu.tsx
@@ -10,7 +10,35 @@ import { useRef, useState } from 'react';
 export default function SettingMoleMenu() {
   const moleImageRef = useRef<HTMLDivElement>(null);
   const [tab, setTab] = useState(1);
-  const [item, setItem] = useState<{ item1: Number, item2: Number, item3: Number, item4: Number }>({ item1: 0, item2: 0, item3: 0, item4: 0 });
+  // const [item, setItem] = useState<{ item1: Number, item2: Number, item3: Number, item4: Number }>({ item1: 0, item2: 0, item3: 0, item4: 0 });
+  const [itemObj, setItemObj] = useState(
+    [
+      {
+        key: 1,
+        name: '눈',
+        pos: { x: 0, y: 0 },
+        image: 1,
+      },
+      {
+        key: 2,
+        name: '머리',
+        pos: { x: 100, y: 0 },
+        image: 1,
+      },
+      {
+        key: 3,
+        name: '옷',
+        pos: { x: 0, y: 100 },
+        image: 1,
+      },
+      {
+        key: 4,
+        name: '장식',
+        pos: { x: 100, y: 100 },
+        image: 1,
+      },
+    ]
+  );
 
   const onDownload = () => {
     if (!moleImageRef.current) return
@@ -19,14 +47,11 @@ export default function SettingMoleMenu() {
     });
   };
 
-  const tmpItemMenu = ['눈', '머리', '옷', '장식'];
-  const itemPos = [[0, 0], [0, 100], [100, 0], [100, 100]]
-
   const tmpItemList = (menu: Number) => {
     let arr = [];
     for (let i = 1; i <= 5; i++) {
       arr.push(
-        <div className='w-[80px] h-[80px] bg-blue-100' onClick={() => setItem((item) => ({ ...item, ['item' + menu]: i }))}>
+        <div className='w-[80px] h-[80px] bg-blue-100' onClick={() => setItemObj((item) => ({ ...item, ['item' + menu]: i }))}>
           <Image src={`/moleItem/type${menu}/${menu}-${i}.png`} width={80} height={80} alt={`item${menu}-${i}`} />
         </div>
       )
@@ -38,23 +63,24 @@ export default function SettingMoleMenu() {
     <div>
       <div ref={moleImageRef} className='moleImage relative'>
         <Image src='/mole.png' width={300} height={300} alt='mole' />
-
         {
-          itemPos.map(([x, y], i) => (
-            <div key={i} className='absolute' style={{ top: `${y}px`, left: `${x}px` }}>
+          itemObj.map((item, i) => (
+            <div key={i} className='absolute' style={{ top: `${item.pos.y}px`, left: `${item.pos.x}px` }}>
               <div className={`w-[80px] h-[80px] bg-blue-100`}>
-                {item.item1 != 0 && <Image src={`/moleItem/type1/1-${item.item1}.png`} width={80} height={80} alt={`item1-${item.item1}`} />}
+                {
+                  item.image != 0
+                  && <Image src={`/moleItem/type${i + 1}/${i + 1}-${item.image}.png`} width={80} height={80} alt={`item${i + 1}-${item.image}`} />
+                }
               </div>
             </div>
           ))
         }
-
       </div>
 
       <div className='flex mt-3 items-center h-10'>
         {
-          tmpItemMenu.map((menu, i) => (
-            <div key={i} className='w-[80px] h-full bg-red-100' onClick={() => setTab(i)}>{menu}</div>
+          itemObj.map((item, i) => (
+            <div key={i} className='w-[80px] h-full bg-red-100' onClick={() => setTab(i)}>{item.name}</div>
           ))
         }
       </div>
