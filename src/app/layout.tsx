9b59de58538f2c0ec85a575c27f0223f3a0fc9ee
jhlee <wisdom_13_@naver.com>
2023-12-25 13:23:01 +0900

    메인 폰트 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import AuthContext from '@/context/AuthContext'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import SWRConfigContext from '@/context/SWRConfigContext'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: '두더지집',
@@ -18,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <AuthContext>
           <div className='w-[390px] m-auto h-screen border border-gray-100'>
             <SWRConfigContext>{children}</SWRConfigContext>
